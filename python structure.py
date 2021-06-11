a5eda4f961f353ac029d07d0e1f2288cc5cacbb9
awal-ahmed <50808571+awal-ahmed@users.noreply.github.com>
2021-06-11 10:55:11 +0600

    Second commit

---
+++ python structure.py
@@ -0,0 +1,14 @@
+
+# Data types
+integer = 5                     #int
+real = 21.5                     #float
+word = "My first program"       #str/ string
+boolean = True                  #bool
+#boolean = False
+
+print(type(boolean))
+# Change the variables to see their type eg. integer, real, word, boolean
+
+
+
+# We can change type of 
